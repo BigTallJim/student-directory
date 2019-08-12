f2068b834c3bc109995f086502da4f3bfa01d5cc
James Holton <james_holton@yahoo.co.uk>
2019-08-12 10:26:31 +0000

    Made the input to the program interactive

--- lib/directory.rb
+++ lib/directory.rb
@@ -31,7 +31,27 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    #students << {name: name, cohort: :november}
+    students.push({name: name, cohort: :november})
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
 #calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
