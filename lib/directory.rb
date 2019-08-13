dfa52122cef798a4440880779c3ccf16916f3da3
James Holton <james_holton@yahoo.co.uk>
2019-08-13 13:43:32 +0000

    Added interactive menu to the directory program

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,18 +1,3 @@
-# All the students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 # prints out the titles  
 def print_header
   puts "The students of Villains Academy"
@@ -49,8 +34,30 @@ def input_students
   students
 end
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 #calling the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
